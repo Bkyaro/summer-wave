a8fc0389c3950433451a40064bb86839a61b8777
Jackal_lzt <bkyarokun@gmail.com>
2024-04-26 15:51:47 +0800

    feat: test cemera settings

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import Scene from "./components/Scene/Scene";
 
 function App() {
 	return (
-		<Canvas>
+		<Canvas camera={{
+      fov: 45,
+      near: 0.1,
+      far: 300,
+      position: [0, 4, -4]
+    }}>
 			<Scene></Scene>
 		</Canvas>
 	);
