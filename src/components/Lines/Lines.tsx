83200569c0b3587fcfb1c525c8ce1d3f45e4c279
Jackal_lzt <bkyarokun@gmail.com>
2024-04-26 16:50:01 +0800

    feat: add many lines

--- src/components/Lines/Lines.tsx
+++ src/components/Lines/Lines.tsx
@@ -1,25 +1,36 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import * as THREE from "three";
 
 const Lines = () => {
-	const linesRef = useRef(null!);
+	const linesRef = useRef<{ children: THREE.Line[] }>(null!);
 
 	useEffect(() => {
 		if (!linesRef.current) return;
-		const line = linesRef.current as THREE.Line;
-		const geometry = line.geometry;
-		const points = [
-			new THREE.Vector3(-1, 0, 0),
-			new THREE.Vector3(1, 0, 1),
-		];
-		geometry.setFromPoints(points);
+		linesRef.current.children.forEach((line) => {
+			const geometry = line.geometry;
+			const points = [
+				new THREE.Vector3(-1, 0, 0),
+				new THREE.Vector3(1, 0, 0),
+			];
+			geometry.setFromPoints(points);
+		});
 	}, []);
+
+	const linesCount = useMemo(() => 69, []);
+	const linesList = useMemo(() => [...new Array(linesCount)], [linesCount]);
+
 	return (
 		<>
-			<line ref={linesRef}>
-				<bufferGeometry />
-				<lineBasicMaterial />
-			</line>
+			<group ref={linesRef} position-z={-linesCount / 20 / 2}>
+				{linesList.map((_, index) => {
+					return (
+						<line position-z={index / 20}>
+							<bufferGeometry />
+							<lineBasicMaterial />
+						</line>
+					);
+				})}
+			</group>
 		</>
 	);
 };
