fa757e92ebd3e882d357b056905dd1c1879ca932
Jackal_lzt <bkyarokun@gmail.com>
2024-04-26 16:58:23 +0800

    feat: oops, line doesnt work properly, switch to boxgeometry

--- src/components/Lines/Lines.tsx
+++ src/components/Lines/Lines.tsx
@@ -1,36 +1,27 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo, useRef } from "react";
+import { Center } from "@react-three/drei";
 import * as THREE from "three";
 
 const Lines = () => {
-	const linesRef = useRef<{ children: THREE.Line[] }>(null!);
-
-	useEffect(() => {
-		if (!linesRef.current) return;
-		linesRef.current.children.forEach((line) => {
-			const geometry = line.geometry;
-			const points = [
-				new THREE.Vector3(-1, 0, 0),
-				new THREE.Vector3(1, 0, 0),
-			];
-			geometry.setFromPoints(points);
-		});
-	}, []);
+	const linesRef = useRef(null!);
 
 	const linesCount = useMemo(() => 69, []);
 	const linesList = useMemo(() => [...new Array(linesCount)], [linesCount]);
 
 	return (
 		<>
-			<group ref={linesRef} position-z={-linesCount / 20 / 2}>
-				{linesList.map((_, index) => {
-					return (
-						<line position-z={index / 20}>
-							<bufferGeometry />
-							<lineBasicMaterial />
-						</line>
-					);
-				})}
-			</group>
+			<Center>
+				<group ref={linesRef}>
+					{linesList.map((_, index) => {
+						return (
+							<mesh position-z={-index * 0.095}>
+								<boxGeometry args={[5, 0.03, 0.02]} />
+								<meshBasicMaterial />
+							</mesh>
+						);
+					})}
+				</group>
+			</Center>
 		</>
 	);
 };
