048cec04de5cacd807900897beb815b7f123edce
Jackal_lzt <bkyarokun@gmail.com>
2024-04-26 17:40:06 +0800

    feat: diy shader and add glsl dependencies

--- src/components/Lines/Lines.tsx
+++ src/components/Lines/Lines.tsx
@@ -2,10 +2,13 @@ import { useMemo, useRef } from "react";
 import { Center } from "@react-three/drei";
 import * as THREE from "three";
 
+import vertexShader from "../../shaders/line/vertex.glsl";
+import fragmentShader from "../../shaders/line/fragment.glsl";
+
 const Lines = () => {
 	const linesRef = useRef(null!);
 
-	const linesCount = useMemo(() => 69, []);
+	const linesCount = useMemo(() => 1, []);
 	const linesList = useMemo(() => [...new Array(linesCount)], [linesCount]);
 
 	return (
@@ -15,8 +18,12 @@ const Lines = () => {
 					{linesList.map((_, index) => {
 						return (
 							<mesh position-z={-index * 0.095}>
-								<boxGeometry args={[5, 0.03, 0.02]} />
-								<meshBasicMaterial />
+								<boxGeometry args={[3, 1, 1, 3, 1, 1]} />
+								<shaderMaterial
+									vertexShader={vertexShader}
+									fragmentShader={fragmentShader}
+									wireframe
+								/>
 							</mesh>
 						);
 					})}
