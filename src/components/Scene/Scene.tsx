bab71dc36f9995385b5b23f04d5b78db263c5fd0
Jackal_lzt <bkyarokun@gmail.com>
2024-04-26 16:16:57 +0800

    feat: add background color

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -3,6 +3,8 @@ import { OrbitControls } from "@react-three/drei";
 const Scene = () => {
 	return (
 		<>
+			{/* scene background: new THREE.Color("#021119") */}
+			<color args={["#021119"]} attach="background"></color>
 			<OrbitControls />
 			<mesh>
 				<boxGeometry />
