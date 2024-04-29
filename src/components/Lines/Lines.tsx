a157ebcf1e9363d46704437ff366e3c3ae661f96
Jackal_lzt <bkyarokun@gmail.com>
2024-04-29 14:25:12 +0800

    feat: extract mesh into Line component, pass uOffset with uniform into vertex glsl, achieve random wavy

--- src/components/Lines/Lines.tsx
+++ src/components/Lines/Lines.tsx
@@ -1,8 +1,7 @@
 import { useMemo, useRef } from "react";
 import { Center } from "@react-three/drei";
 
-import vertexShader from "../../shaders/line/vertex.glsl";
-import fragmentShader from "../../shaders/line/fragment.glsl";
+import Line from "../Line/Line";
 
 const Lines = () => {
 	const linesRef = useRef(null!);
@@ -15,17 +14,7 @@ const Lines = () => {
 			<group ref={linesRef}>
 				<axesHelper args={[5]}></axesHelper>
 				{linesList.map((_, index) => {
-					return (
-						<mesh position-z={index * 0.095}>
-							<boxGeometry args={[5, 0.03, 0.02, 128, 1, 1]} />
-
-							<shaderMaterial
-								vertexShader={vertexShader}
-								fragmentShader={fragmentShader}
-								// wireframe
-							/>
-						</mesh>
-					);
+					return <Line index={index} />;
 				})}
 			</group>
 		</Center>
--- src/shaders/line/vertex.glsl
+++ src/shaders/line/vertex.glsl
@@ -1,7 +1,9 @@
 #include ../noise;
 
+uniform float uOffset;
+
 void main(){
     vec3 pos = position;
-    pos.y += noise2D(vec2(pos.x, 0.0) * 50.0) * 0.018;
+    pos.y += noise2D(vec2(pos.x + uOffset, 0.0) * 50.0) * 0.018;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(pos, 1.0);
 }
