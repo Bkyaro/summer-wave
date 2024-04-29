4456559ef1c047ebbd8519d42cb47054f368becd
Jackal_lzt <bkyarokun@gmail.com>
2024-04-29 14:42:57 +0800

    feat: use uv coordinate to do some math, the wave is more accurate now

--- src/shaders/line/vertex.glsl
+++ src/shaders/line/vertex.glsl
@@ -4,6 +4,11 @@ uniform float uOffset;
 
 void main(){
     vec3 pos = position;
+
+    float flatnessAmplitude = pow(1.0 - abs(uv.x - 0.5) * 2.0, 3.5);
+
     pos.y += noise2D(vec2(pos.x + uOffset, 0.0) * 50.0) * 0.018;
+    pos.y += noise2D(vec2(pos.x + uOffset, 0.0) * 1000.0) * 0.1 * flatnessAmplitude;
+    pos.y += pow(noise2D(vec2(pos.x + uOffset, 0.0) * 2.5) + 1.0, 1.2) * 0.55 * flatnessAmplitude;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(pos, 1.0);
 }
