d97d62ca9dab16ea4adb739ab31061212a2ad5ed
Jackal_lzt <bkyarokun@gmail.com>
2024-04-29 17:05:13 +0800

    feat: slow the uTime down

--- src/shaders/line/vertex.glsl
+++ src/shaders/line/vertex.glsl
@@ -8,8 +8,8 @@ void main(){
 
     float flatnessAmplitude = pow(1.0 - abs(uv.x - 0.5) * 2.0, 3.5);
 
-    pos.y += noise2D(vec2(pos.x + uOffset, uTime) * 50.0) * 0.018;
-    pos.y += noise2D(vec2(pos.x + uOffset, uTime) * 1000.0) * 0.1 * flatnessAmplitude;
-    pos.y += pow(noise2D(vec2(pos.x + uOffset, uTime) * 2.5) + 1.0, 1.2) * 0.55 * flatnessAmplitude;
+    pos.y += noise2D(vec2(pos.x + uOffset, uTime * 0.02) * 50.0) * 0.018;
+    pos.y += noise2D(vec2(pos.x + uOffset, uTime * 0.001) * 1000.0) * 0.1 * flatnessAmplitude;
+    pos.y += pow(noise2D(vec2(pos.x + uOffset, uTime * 0.05) * 2.5) + 1.0, 1.2) * 0.55 * flatnessAmplitude;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(pos, 1.0);
 }
