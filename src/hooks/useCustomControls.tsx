7e1d2d6faa1a2dc9e44b623efa03302c7cba983e
Jackal_lzt <bkyarokun@gmail.com>
2024-05-02 14:57:45 +0800

    feat: add line count control panel

--- src/hooks/useCustomControls.tsx
+++ src/hooks/useCustomControls.tsx
@@ -27,7 +27,7 @@ function useAxesControl() {
 		"axes",
 		{
 			showAxes: {
-				value: true,
+				value: false,
 			},
 			AxesLength: {
 				min: 0,
@@ -70,6 +70,18 @@ function useLineControl() {
 				max: 5,
 				step: 0.01,
 				value: 0.095,
+			},
+			count: {
+				min: 1,
+				max: 256,
+				step: 1,
+				value: 69,
+			},
+			uStrength: {
+				min: 0,
+				max: 99,
+				step: 1,
+				value: 1,
 			},
 		},
 		{ collapsed: true }
