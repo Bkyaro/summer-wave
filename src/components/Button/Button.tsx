e44969017a14fb83d698233886c1cd5178ac1ba1
Jackal_lzt <bkyarokun@gmail.com>
2024-05-02 10:47:51 +0800

    feat: add gsap to play button for hiding

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,33 @@
+import { useMemo, useRef } from "react";
+import gsap from "gsap";
+
 import useStore from "../../store/store";
 import { Play } from "../Icons";
 
 const PlayButton = () => {
+	const buttonRef = useRef<HTMLDivElement>(null);
 	const setIsMusicPlaying = useStore((state) => state.setIsMusicPlaying);
+
+	const onClickHandler = useMemo(
+		() => () => {
+			gsap.to(buttonRef.current, {
+				opacity: 0,
+				duration: 1,
+				ease: "power2.in",
+				onComplete: () => {
+					gsap.set(buttonRef.current, {
+						display: "none",
+					});
+					setIsMusicPlaying(true);
+				},
+			});
+		},
+		[setIsMusicPlaying]
+	);
+
 	return (
-		<div id="button-container">
-			<button id="play-btn" onClick={() => setIsMusicPlaying(true)}>
+		<div ref={buttonRef} id="button-container" onClick={onClickHandler}>
+			<button id="play-btn">
 				<Play />
 				<span>LOSE CONTROL</span>
 			</button>
