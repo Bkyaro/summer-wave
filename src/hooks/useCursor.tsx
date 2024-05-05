b3c062e99c0adff2cde3441af5913db496aee358
Jackal_lzt <bkyarokun@gmail.com>
2024-05-05 14:05:40 +0800

    chore: do some typing

--- src/hooks/useCursor.tsx
+++ src/hooks/useCursor.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 
 const useCursor = () => {
 	const cursor = document.getElementById("cursor");
 	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		let cursorFocus: gsap.core.Tween;
 
 		const onMouseMove = (event: MouseEvent) => {
@@ -17,7 +18,7 @@ const useCursor = () => {
 		};
 
 		const onMouseDown = (event: MouseEvent) => {
-			if (event.target.tagName == "CANVAS") {
+			if (event.target && (event.target as Element).tagName == "CANVAS") {
 				cursorFocus = gsap.to(cursor, {
 					scale: 0.6,
 					boxShadow: "inset 0px 0px 10px 2px #fff",
@@ -32,7 +33,10 @@ const useCursor = () => {
 				duration: 0.35,
 				ease: "sine.out",
 			});
-			if (event.target && event.target.tagName === "CANVAS") {
+			if (
+				event.target &&
+				(event.target as Element).tagName === "CANVAS"
+			) {
 				gsap.to(cursor, {
 					scale: 1,
 					boxShadow: "none",
