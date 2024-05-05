c096213c9bccd919a0cc575776826f12287c40f7
Jackal_lzt <bkyarokun@gmail.com>
2024-05-05 11:23:22 +0800

    feat: add unselectable style & add cursor focus animation

--- src/hooks/useCursor.tsx
+++ src/hooks/useCursor.tsx
@@ -1,23 +1,55 @@
 import { useEffect } from "react";
-
 import gsap from "gsap";
 
 const useCursor = () => {
 	const cursor = document.getElementById("cursor");
 	useEffect(() => {
+		let cursorFocus: gsap.core.Tween;
+
 		const onMouseMove = (event: MouseEvent) => {
 			cursor &&
 				gsap.to(cursor, {
 					x: event.clientX,
 					y: event.clientY,
-					duration: 0.28,
+					duration: 0.1,
 					ease: "sine.out",
 				});
 		};
 
+		const onMouseDown = (event: MouseEvent) => {
+			if (event.target.tagName == "CANVAS") {
+				cursorFocus = gsap.to(cursor, {
+					scale: 0.6,
+					boxShadow: "inset 0px 0px 10px 2px #fff",
+					duration: 0.35,
+				});
+			}
+		};
+
+		const onMouseUp = (event: MouseEvent) => {
+			gsap.to(cursor, {
+				boxShadow: "none",
+				duration: 0.35,
+				ease: "sine.out",
+			});
+			if (event.target && event.target.tagName === "CANVAS") {
+				gsap.to(cursor, {
+					scale: 1,
+					boxShadow: "none",
+					duration: 0.35,
+					ease: "sine.out",
+				});
+			}
+		};
+
 		window.addEventListener("mousemove", onMouseMove);
+		window.addEventListener("mousedown", onMouseDown);
+		window.addEventListener("mouseup", onMouseUp);
+
 		return () => {
 			window.removeEventListener("mousemove", onMouseMove);
+			window.removeEventListener("mousedown", onMouseDown);
+			window.removeEventListener("mouseup", onMouseUp);
 		};
 	}, []);
 };
